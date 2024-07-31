22e3c3f72c5d5d05ca0f8f68620999c8b86806e1
Abhilash Gowda <abhilashgowda278@gmail.com>
2024-07-31 21:17:37 +0530

    Create User.java

---
+++ src/main/java/user/User.java
@@ -0,0 +1,48 @@
+package main.java.user;
+
+public class User {
+
+    private final String id;
+    private final String name;
+    private final String email;
+
+    public User(String id, String name, String email) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id='" + id + '\'' +
+                ", name='" + name + '\'' +
+                ", email='" + email + '\'' +
+                '}';
+    }
+
+//    @Override
+//    public int hashCode() {
+//        return super.hashCode();
+//    }
+//
+//    @Override
+//    public boolean equals(Object obj) {
+//        return super.equals(obj);
+//    }
+
+
+
+}
